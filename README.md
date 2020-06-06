09f1305f934c21b3eedf022984adf2d09b0ac978
Juan Bonforti <juan.bonforti@gmail.com>
2020-06-06 11:04:17 -0300

    Swift 4 - Multiline Label.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Curso iOS - Swift 4
+Section 30, donde explican como realizar labels multilinea.
--- Swift4-Section30/ViewController.swift
+++ Swift4-Section30/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var etiquetaMultiple: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        etiquetaMultiple.backgroundColor = .black
+        etiquetaMultiple.textColor = .white
+        etiquetaMultiple.font = UIFont.boldSystemFont(ofSize: 32); // negrita
+        
+        // Configuraremos la cantidad de lineas posibles en el label. Para que acepte N, podemos dejarle el valor 0. Sino, especificamos las cantidades deseadas.
+        etiquetaMultiple.numberOfLines = 0
+        
+        // Alineando el texto
+        etiquetaMultiple.textAlignment = .center
+        
+        // Para hacer multilinea se debe comenzar y terminar con """. Si queremos obviar la multiple linea en ciertos caracteres, le concatenamos un \
+        etiquetaMultiple.text = """
+        Hola
+        mundo
+        """
     }
-
 
 }
 
